594e7365364e526a81311e2e5b63cf7c704da53c
권예림 <124475716+yerim07@users.noreply.github.com>
2024-05-26 21:29:52 +0900

    feat : 일정 목록 페이지 일정 삭제
    일정 삭제 기능 하고 있음.
    라우터 오류남.
    라우터 오류로 기능이 되는지 확인이 안됨.

--- click/src/components/ScheduleInquiryPage/scheduleInquiryPage.jsx
+++ click/src/components/ScheduleInquiryPage/scheduleInquiryPage.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useMemo, useReducer, useCallback, useRef } from "react";
 import * as L from "./scheduleInquiryPage.style";
+import ScheduleList from "./ScheduleList";
 
-const ScheduleInquiryPage = () => {
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createdDate: new Date().getTime(),
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE": {
+      return [action.newItem, ...state];
+    }
+    case "UPDATE": {
+      return state.map((it) =>
+        it.id === action.targetId
+          ? {
+              ...it,
+              isDone: !it.isDone,
+            }
+          : it
+      );
+    }
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    default:
+      return state;
+  }
+}
+
+export const ScheduleContext = React.createContext();
+export const ScheduleStateContext = React.createContext();
+export const ScheduleDispatchContext = React.createContext();
+
+function ScheduleInquiryPage() {
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
+  const idRef = useRef(3);
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      newItem: {
+        id: idRef.current,
+        content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
+    idRef.current += 1;
+  };
+
+  const onUpdate = useCallback((targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId,
+    });
+  }, []);
+
+  const onDelete = useCallback((targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  }, []);
+
+  const memoizedDispatches = useMemo(() => {
+    return { onCreate, onUpdate, onDelete };
+  });
   return (
     <>
       <L.FullScreenColor>
@@ -12,76 +93,17 @@ const ScheduleInquiryPage = () => {
             <L.Select>선택</L.Select>
             <L.Title>제목</L.Title>
           </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
-          <div>
-            <L.LineG />
-            <L.Checkbox type="checkbox" />
-            <L.MainT>
-              2099년 13월 45일 15교시 2학년 6반 실용 국어 내가 읽은 책 소개하기
-              1회차
-            </L.MainT>
-          </div>
+          <ScheduleStateContext.Provider value={todo}>
+            <ScheduleDispatchContext.Provider value={memoizedDispatches}>
+              <ScheduleList />
+            </ScheduleDispatchContext.Provider>
+          </ScheduleStateContext.Provider>
           <L.LineG />
           <L.Delete>삭제</L.Delete>
         </div>
       </L.FullScreenColor>
     </>
   );
-};
+}
 
 export default ScheduleInquiryPage;
