71a59fb8e0d578266bd6cf0a7964cdb158926160
leehak07 <leehak1228@dgsw.hs.kr>
2024-06-02 15:04:24 +0900

    teacherMain
    캘린더안돼요....

--- click/src/components/TeacherMain/teacherMain.jsx
+++ click/src/components/TeacherMain/teacherMain.jsx
@@ -3,106 +3,106 @@ import Header from './Header/teacherHeader';
 import React, { useState, useEffect, useRef } from 'react';
 
 const teacherMain = () => {
-  const [date, setDate] = useState(new Date());
-  const yearMonthRef = useRef(null);
-  const datesRef = useRef(null);
-  const modalRef = useRef(null);
+  // const [date, setDate] = useState(new Date());
+  // const yearMonthRef = useRef(null);
+  // const datesRef = useRef(null);
+  // const modalRef = useRef(null);
 
-  useEffect(() => {
-    renderCalendar();
-  }, [date]);
+  // useEffect(() => {
+  //   renderCalendar();
+  // }, [date]);
 
-  const renderCalendar = () => {
-    const viewYear = date.getFullYear();
-    const viewMonth = date.getMonth();
+  // const renderCalendar = () => {
+  //   const viewYear = date.getFullYear();
+  //   const viewMonth = date.getMonth();
 
-    if (yearMonthRef.current) {
-      yearMonthRef.current.textContent = `${viewYear}년 ${viewMonth + 1}월`;
-    }
+  //   if (yearMonthRef.current) {
+  //     yearMonthRef.current.textContent = `${viewYear}년 ${viewMonth + 1}월`;
+  //   }
 
-    const prevLast = new Date(viewYear, viewMonth, 0);
-    const thisLast = new Date(viewYear, viewMonth + 1, 0);
+  //   const prevLast = new Date(viewYear, viewMonth, 0);
+  //   const thisLast = new Date(viewYear, viewMonth + 1, 0);
 
-    const PLDate = prevLast.getDate();
-    const PLDay = prevLast.getDay();
+  //   const PLDate = prevLast.getDate();
+  //   const PLDay = prevLast.getDay();
 
-    const TLDate = thisLast.getDate();
-    const TLDay = thisLast.getDay();
+  //   const TLDate = thisLast.getDate();
+  //   const TLDay = thisLast.getDay();
 
-    const prevDates = [];
-    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
-    const nextDates = [];
+  //   const prevDates = [];
+  //   const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+  //   const nextDates = [];
 
-    if (PLDay !== 6) {
-      for (let i = 0; i < PLDay + 1; i++) {
-        prevDates.unshift(PLDate - i);
-      }
-    }
+  //   if (PLDay !== 6) {
+  //     for (let i = 0; i < PLDay + 1; i++) {
+  //       prevDates.unshift(PLDate - i);
+  //     }
+  //   }
 
-    for (let i = 1; i < 7 - TLDay; i++) {
-      nextDates.push(i);
-    }
+  //   for (let i = 1; i < 7 - TLDay; i++) {
+  //     nextDates.push(i);
+  //   }
 
-    const dates = prevDates.concat(thisDates, nextDates);
-    const firstDateIndex = dates.indexOf(1);
-    const lastDateIndex = dates.lastIndexOf(TLDate);
+  //   const dates = prevDates.concat(thisDates, nextDates);
+  //   const firstDateIndex = dates.indexOf(1);
+  //   const lastDateIndex = dates.lastIndexOf(TLDate);
 
-    const formattedDates = dates.map((date, i) => {
-      const condition = i >= firstDateIndex && i < lastDateIndex + 1 ? "this" : "other";
-      return `<div class="date"><span class="${condition}">${date}</span></div>`;
-    });
+  //   const formattedDates = dates.map((date, i) => {
+  //     const condition = i >= firstDateIndex && i < lastDateIndex + 1 ? "this" : "other";
+  //     return `<div class="date"><span class="${condition}">${date}</span></div>`;
+  //   });
 
-    if (datesRef.current) {
-      datesRef.current.innerHTML = formattedDates.join("");
-    }
+  //   if (datesRef.current) {
+  //     datesRef.current.innerHTML = formattedDates.join("");
+  //   }
 
-    const today = new Date();
-    if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
-      const todayElement = datesRef.current.querySelector('.this.today');
-      if (todayElement) {
-        todayElement.classList.remove('today');
-      }
-      const todayDates = datesRef.current.querySelectorAll(".this");
-      for (let date of todayDates) {
-        if (+date.innerText === today.getDate()) {
-          date.classList.add("today");
-          break;
-        }
-      }
-    }
-  };
+  //   const today = new Date();
+  //   if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+  //     const todayElement = datesRef.current.querySelector('.this.today');
+  //     if (todayElement) {
+  //       todayElement.classList.remove('today');
+  //     }
+  //     const todayDates = datesRef.current.querySelectorAll(".this");
+  //     for (let date of todayDates) {
+  //       if (+date.innerText === today.getDate()) {
+  //         date.classList.add("today");
+  //         break;
+  //       }
+  //     }
+  //   }
+  // };
 
-  const prevMonth = () => {
-    setDate(new Date(date.getFullYear(), date.getMonth() - 1));
-  };
+  // const prevMonth = () => {
+  //   setDate(new Date(date.getFullYear(), date.getMonth() - 1));
+  // };
 
-  const nextMonth = () => {
-    setDate(new Date(date.getFullYear(), date.getMonth() + 1));
-  };
+  // const nextMonth = () => {
+  //   setDate(new Date(date.getFullYear(), date.getMonth() + 1));
+  // };
 
-  const goToday = () => {
-    setDate(new Date());
-  };
+  // const goToday = () => {
+  //   setDate(new Date());
+  // };
 
-  const openModal = () => {
-    console.log('open')
-    if (modalRef.current) {
-      modalRef.current.classList.add('on');
-    }
-    setModalOpen(true)
-  };
+  // const openModal = () => {
+  //   console.log('open')
+  //   if (modalRef.current) {
+  //     modalRef.current.classList.add('on');
+  //   }
+  //   setModalOpen(true)
+  // };
 
-  const closeModal = () => {
-    if (modalRef.current) {
-      modalRef.current.classList.remove('on');
-    }
-  };
+  // const closeModal = () => {
+  //   if (modalRef.current) {
+  //     modalRef.current.classList.remove('on');
+  //   }
+  // };
 
-  const [modalOpen, setModalOpen] = useState(false);
+  // const [modalOpen, setModalOpen] = useState(false);
 
-  const eventModalOpen = () => {
-    setModalOpen(!modalOpen);
-  }
+  // const eventModalOpen = () => {
+  //   setModalOpen(!modalOpen);
+  // }
 
 
     return(
