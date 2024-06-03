f7c1eb9268bc48c6cb197e3e26517595768f6e8c
권예림 <124475716+yerim07@users.noreply.github.com>
2024-06-03 18:50:05 +0900

    style : 라우터
    라우터 주석 처리

--- click/src/Router.jsx
+++ click/src/Router.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
 
-import TeacherPage from "./components/TeacherPage/TeacherPage";
-import ScheduleInquiryPage from "./components/SchedulelnquiryPage/SchedulelnquiryPage";
-import CorrectionPage from "./components/CorrectionPage/CorrectionPage";
+// import TeacherPage from "./components/TeacherPage/TeacherPage";
+// import ScheduleInquiryPage from "./components/SchedulelnquiryPage/SchedulelnquiryPage";
 // import CorrectionPage from "./components/CorrectionPage/CorrectionPage";
+// // import CorrectionPage from "./components/CorrectionPage/CorrectionPage";
 
-function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<TeacherPage />} />
-      <Route path="/ScheduleInquiryPage" element={<ScheduleInquiryPage />} />
-      <Route path="/CorrectionPage" element={<CorrectionPage />} />
-    </Routes>
-  );
-}
+// function Router() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<TeacherPage />} />
+//       <Route path="/ScheduleInquiryPage" element={<ScheduleInquiryPage />} />
+//       <Route path="/CorrectionPage" element={<CorrectionPage />} />
+//     </Routes>
+//   );
+// }
 
-export default Router;
+// export default Router;
