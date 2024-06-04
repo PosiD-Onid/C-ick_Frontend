7b9e7d91fa59d7a3655f980246890dc1ea216b61
leehak07 <leehak1228@dgsw.hs.kr>
2024-06-04 09:50:00 +0900

    Signup, Login, Main
    Signup, Login 수정하고
    Main js코드 지웠어욤

--- click/src/components/studentSignup/studentSignup.jsx
+++ click/src/components/studentSignup/studentSignup.jsx
@@ -1,17 +1,16 @@
-import "./teacherSignup.css";
+import "./studentSignup.css";
 import React, { useState } from "react";
 
 const type = {
     userId: "",
     password: "",
     passwordCheck: "",
-    subject: "",
   };
   
   
 function Signup() {
     const [data, setData] = useState(type);
-    const {userId, password, passwordCheck, subject} = data;
+    const {userId, password, passwordCheck} = data;
 
     const onChangeInput = (e) => {
         const {value, name: inputName} = e.target;
@@ -28,19 +27,17 @@ function Signup() {
         password !== passwordCheck ? alert("비밀번호를 다시 입력해 주세요")
         : alert("회원가입 성공") /** 페이지 라우팅... */;
     }   
-
+    
     return(
         <div>
             <h2 className="text">회원가입</h2>
             <p>* 도담도담 아이디</p>
-            <input type="text"  name='userId' value={userId} onChange={onChangeInput} placeholder="아이디" className="input"></input><br />
+            <input type="text"  name='userId' value={userId} onChange={onChangeInput} placeholder="아이디" className="inputId"></input><br/>
             <button type="submit" name="IdCheck" onClick={onClickIdCheck}>중복 확인</button><br />
             <p>* 도담도담 비밀번호</p>
-            <input type="password" name='password' value={password} onChange={onChangeInput} placeholder="비밀번호" className="input"></input><br />
+            <input type="password" name='password' value={password} onChange={onChangeInput} placeholder="비밀번호" className="IdCheck"></input><br />
             <p>* 비밀번호 확인</p>
             <input type="passwordCheck" name='passwordCheck' value={passwordCheck} onChange={onChangeInput} placeholder="비밀번호" className="input"></input><br />
-            <p>* 과목명</p>
-            <input type="text"  name='subject' value={subject} onChange={onChangeInput} placeholder="과목" className="input"></input><br/>
             <button type="submit" className="Signupbtn"
             onClick={onClickSignup}
             >회원가입</button>
